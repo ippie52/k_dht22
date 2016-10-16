1489c92c1c2e52f9c28ff71aa5f2e0777315277c
Kris Dunning <ippie52@gmail.com>
2016-10-16 22:06:18 +0000

    Add feature to check previous readings
    Feature allows the last valid readings to be stored, and in turn read
    the next time the application is run. This is then used in conjunction
    with the evaluate method to check the consistency.
    
    This has been added to confront the issue found where it appears that
    some bits are being shifted in such a way that the values are not
    correct, but the checksum is still acceptable. This is often expressed
    as doubled humidity and temperature values, which can be identified
    by the humidity going above the 100% value, but this is not always
    the case.
    
    If the last stored values are inconsistent, the current reading will be
    stored, then once another reading is obtained, this will be compared
    firstly to the stored value, and then to the previously read value.
    
    Assumptions made:
    * Temperature and Humidity values should not change more than 5*c or 5%
    between the current value and the last read value.
    * Readings are made every minute.
    * If two values read during the current evaluation both disagree with
    the stored value, assume the readings are now correct and they have
    changed significantly since the last stored value.
    
    Change-Id: I2ab12f43049bdd37ddf65cc5d142c26d909abc4b

--- dht22.c
+++ dht22.c
@@ -20,7 +20,8 @@
 #include "locking.h"
 
 static const float MAX_HUMIDITY = 99.9f;
-static const int MAX_LOCKING_PATH_LENGTH = 100;
+static const int MAX_PATH_LENGTH = 100;
+static const int MAX_READING_LENGTH = 20;
 static const int DEFAULT_PIN = 7;
 static const int MAX_TIMINGS = 85;
 
@@ -29,10 +30,11 @@ static const int MAX_TIMINGS = 85;
 // -----------------------------------------------------------------------------
 typedef enum Results
 {
-    RESULT_OK,        /* Valid values appear to have been found */
-    RESULT_BAD_DATA,  /* Bad data                               */
-    RESULT_ALL_ZERO,  /* All values are zero - suspicious       */
-    RESULT_INVALID,   /* Data appears to be invalid             */
+    RESULT_OK,          /* Valid values appear to have been found   */
+    RESULT_BAD_DATA,    /* Bad data                                 */
+    RESULT_ALL_ZERO,    /* All values are zero - suspicious         */
+    RESULT_INCONSISTENT,/* Data inconsistent from last reading      */
+    RESULT_INVALID,     /* Data appears to be invalid               */
 } SensorReadingResults;
 
 // -----------------------------------------------------------------------------
@@ -46,6 +48,8 @@ typedef struct Values
 
 } SensorValues;
 
+#define INVALID_VALUES { RESULT_INVALID, 0.0f, 0.0f }
+
 /*******************************************************************************
  *  \brief  Evaluates the sensor values to sanity check the results found.
  *  \return SensorReadingResults value to indicate the legitimacy of the results
@@ -53,7 +57,7 @@ typedef struct Values
  */
 static int evaluate
 (
-    const SensorValues * const values ///<IN - The SensorValues to evaluate
+    const SensorValues * const values   ///<IN - The SensorValues to evaluate
 )
 {
     if (MAX_HUMIDITY < values->humidity)
@@ -69,6 +73,49 @@ static int evaluate
     }
 
     return RESULT_OK;
+}
+
+/*******************************************************************************
+ *  \brief  Evaluates the sensor values to sanity check the results found
+ *          against previous read and stored values.
+ *  \return SensorReadingResults value to indicate the legitimacy of the results
+ *          obtained.
+ */
+static int evaluate_last
+(
+    const SensorValues last_stored, ///<IN - The last SensorValues stored on file
+    SensorValues *values,           ///<IN/OUT - The SensorValues to evaluate
+    SensorValues *last_read         ///<OUT - The last read values for comparison
+)
+{
+    values->result = evaluate(values);
+    if (RESULT_OK == values->result && RESULT_OK == last_stored.result)
+    {
+        // First, let's check whether its similar enough
+        if (abs(last_stored.temperature - values->temperature) > 5.0f ||
+            abs(last_stored.humidity - values->humidity) > 5.0f)
+        {
+            // Now, let's check to see whether we have a previous reading, and if so, whether
+            // the temperature or humidity has genuinely changed this much
+            if (RESULT_INCONSISTENT == last_read->result &&
+                abs(last_read->temperature - values->temperature) < 5.0f &&
+                abs(last_read->humidity - values->humidity) < 5.0f)
+            {
+                fprintf(stderr, "Last two read values appear to match, ignoring saved inconsistency");
+                // We can assume the value(s) have actually changed this much
+                values->result = RESULT_OK;
+            }
+            else
+            {
+                fprintf(stderr, "Last value seems inconsistent, reading again\n");
+                // Either the value doesn't match up, keep trying, or this is the first check
+                values->result = RESULT_INCONSISTENT;
+            }
+        }
+        *last_read = *values;
+    }
+
+    return values->result;
 }
 
 /*******************************************************************************
@@ -93,13 +140,112 @@ static uint8_t sizecvt
 }
 
 /*******************************************************************************
+ *  \brief  This returns the file pointer to a stored sensor file
+ *  \return The file pointer to the sensor reading file, NULL if fail.
+ */
+static FILE *get_sensor_file_descriptor
+(
+    const int sensor_pin,   ///<IN - The sensor pin ID used to identify the file
+    const char *open_mode   ///<IN - The file open mode, i.e. "r+"
+)
+{
+    char filename[MAX_PATH_LENGTH];
+    FILE *fp = NULL;
+    snprintf(filename, MAX_PATH_LENGTH, "/tmp/dhtsensor.%d", sensor_pin);
+    fp = fopen(filename, open_mode);
+    if (NULL == fp)
+    {
+        fprintf(stderr, "Failed to open file %s in mode \"%s\".\n",
+            filename, open_mode);
+    }
+    return fp;
+}
+
+/*******************************************************************************
+ *  \brief  Gets the last sensor values, if stored.
+ *  \return Returns the SensorValues interpreted from the saved values.
+ */
+static SensorValues get_last_values
+(
+    const int sensor_pin    ///<IN - The sensor pin to check
+)
+{
+    SensorValues values = INVALID_VALUES;
+    char contents[MAX_READING_LENGTH];
+    size_t size = 0L;
+    FILE *fp = get_sensor_file_descriptor(sensor_pin, "r");
+    if (fp)
+    {
+        fseek(fp, 0L, SEEK_END);
+        size = ftell(fp);
+        rewind(fp);
+        int read_pin;
+        int read_temp;
+        int read_hum;
+        if (size < MAX_READING_LENGTH)
+        {
+            if (fread(contents, size, 1, fp) <= size)
+            {
+                sscanf(contents, "%d %d %d", &read_pin, &read_temp, &read_hum);
+                if (read_pin != sensor_pin)
+                {
+                    fprintf(stderr, "Read pin does not match expected: %d != %d\n",
+                        read_pin, sensor_pin);
+                }
+                else
+                {
+                    values.temperature = (float)(read_temp) / 1000.0f;
+                    values.humidity = (float)(read_hum) / 1000.0f;
+                    values.result = evaluate(&values);
+                }
+            }
+            else
+            {
+                fprintf(stderr, "Problem reading contents of sensor file\n");
+            }
+        }
+        fclose(fp);
+    }
+    return values;
+}
+
+/*******************************************************************************
+ *  \brief  Stores the last sensor values.
+ *  \return Zero if storing fails, otherwise 1.
+ */
+static int set_last_values
+(
+    const int sensor_pin,       ///<IN - The sensor pin to store
+    const SensorValues values   ///<IN - The sensor value readings to store
+)
+{
+    int result = 0;
+    FILE *fp = get_sensor_file_descriptor(sensor_pin, "w");
+    if (fp)
+    {
+        fprintf(fp, "%d %06d %06d",
+            sensor_pin,
+            (int)(1000 * (values.temperature + 0.5f)),
+            (int)(1000 * (values.humidity + 0.5f)));
+        fclose(fp);
+        result = 1;
+    }
+    else
+    {
+        fprintf(stderr, "Error: Could not write to file.\n");
+    }
+    return result;
+}
+
+/*******************************************************************************
  *  \brief  Reads the DHT22 value and returns the result of the read.
  *  \return The SensorReadingResults value.
  */
 static SensorReadingResults read_dht22_data
 (
-    const int sensor_pin,   ///<IN - The sensor pin to read
-    SensorValues *values    ///<OUT - The values to set
+    const int sensor_pin,           ///<IN - The sensor pin to read
+    SensorValues *values,           ///<OUT - The values to set
+    const SensorValues last_stored  ///<IN - The last stored values
 )
 {
     uint8_t laststate = HIGH;
@@ -107,6 +253,7 @@ static SensorReadingResults read_dht22_data
     uint8_t j = 0, i;
     int data_sum = 0;
     static int dht22_data[5] = { 0, 0, 0, 0, 0 };
+    static SensorValues last_read = INVALID_VALUES;
 
     memset(dht22_data, 0, sizeof(dht22_data));
 
@@ -157,8 +304,7 @@ static SensorReadingResults read_dht22_data
 
     // Check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
     data_sum = (dht22_data[0] + dht22_data[1] + dht22_data[2] + dht22_data[3]);
-    if ((j >= 40) &&
-    (dht22_data[4] == (uint8_t)(data_sum & 0xFF)))
+    if ((j >= 40) && (dht22_data[4] == (uint8_t)(data_sum & 0xFF)))
     {
         values->humidity = (float)dht22_data[0] * 256 + (float)dht22_data[1];
         values->humidity /= 10;
@@ -169,7 +315,7 @@ static SensorReadingResults read_dht22_data
         {
           values->temperature *= -1.0;
         }
-        values->result = evaluate(values);
+        values->result = evaluate_last(last_stored, values, &last_read);
     }
     else
     {
@@ -193,7 +339,7 @@ int main
     int dht_pin = DEFAULT_PIN;
     int zero_count = 0;
     int tries = 100;
-    char buffer[MAX_LOCKING_PATH_LENGTH];
+    char buffer[MAX_PATH_LENGTH];
 
     if (argc < 2)
     {
@@ -219,7 +365,7 @@ int main
         exit(EXIT_FAILURE);
     }
 
-    get_lockfile_name(dht_pin, buffer, MAX_LOCKING_PATH_LENGTH);
+    get_lockfile_name(dht_pin, buffer, MAX_PATH_LENGTH);
     lockfd = open_lockfile(buffer);
 
     if (wiringPiSetup() == -1)
@@ -234,12 +380,16 @@ int main
         exit(EXIT_FAILURE);
     }
 
-    fprintf(stderr, "Beginning read attempts\n");
-    SensorValues values = { 0 };
+    SensorValues last_stored = get_last_values(dht_pin);
+    if (RESULT_OK != last_stored.result)
+    {
+        fprintf(stderr, "Stored results were not OK, ignoring them.\n");
+    }
+
+    SensorValues values = INVALID_VALUES;
     while (tries--)
     {
-        fprintf(stderr, "Reading values...\n");
-        if (read_dht22_data(dht_pin, &values) == RESULT_ALL_ZERO)
+        if (read_dht22_data(dht_pin, &values, last_stored) == RESULT_ALL_ZERO)
         {
             fprintf(stderr, "Reading was zero, checking again\n");
             ++zero_count;
@@ -270,6 +420,8 @@ int main
     {
         fprintf(stderr, "Values could not be obtained.\n");
     }
+
+    set_last_values(dht_pin, values);
 
     delay(1500);
     close_lockfile(lockfd);
